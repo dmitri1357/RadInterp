e09319043e69f52d857daa41f4d74041045e83d7
Dmitri Kalashnikov <48308024+dmitri1357@users.noreply.github.com>
2019-09-28 10:56:48 -0700

    Update pol2cart.py

--- pol2cart.py
+++ pol2cart.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def pol2cart(theta, rho):
     """
-    A simple function to convert from polar to cartesian (x,y) coordinates, in the style
-    of MATLAB 'pol2cart' function.
+    A simple function to convert from polar to cartesian (x,y) coordinates, in
+    the style of MATLAB 'pol2cart' function.
 
     Parameters
     ----------
@@ -13,12 +13,10 @@ def pol2cart(theta, rho):
 
     Returns
     -------
-    x, y :  Cartesian (x,y) coordinates corresponding to input polar coordinates.
+    x, y :  Cartesian (x,y) coordinates corresponding to input polar
+            coordinates.
     """
 
     x = rho * np.cos(theta)
     y = rho * np.sin(theta)
     return(x,y)
-
-
-
