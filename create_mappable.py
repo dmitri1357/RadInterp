6953ed2ae7bced70acc51932b2738a0569e5aa6b
Dmitri Kalashnikov <48308024+dmitri1357@users.noreply.github.com>
2019-10-22 15:20:45 -0700

    Update create_mappable.py

--- create_mappable.py
+++ create_mappable.py
@@ -29,18 +29,20 @@ def create_mappable(radius_steps, degree_steps, interp_vals):
         for contourf plotting.
     """
 
-    assert interp_vals.ndim == 1, """
-Input data values must be 1D"""
+    assert interp_vals.ndim == 1, "Input data values must be 1D"
     assert radius_steps[0] == 0, """
-Starting radius must be zero for contour mappable"""
+Starting radius must be zero for contourf mappable"""
 
     rho = radius_steps/radius_steps[-1] # radius values on unit circle (r = 1)
+
     # vector of rho values corresponding to lat & lon points
     rho_vec = np.repeat(rho,np.size(degree_steps))
 
     theta = np.deg2rad(degree_steps) # convert to radians
+
     # vector of theta values corresponding to lat & lon points
     theta_vec = np.tile(theta,np.size(radius_steps))
+
     # Calculate index for all (x,y) coordinates.
     xi = np.tile(degree_steps,np.size(radius_steps)).astype('int32')
     yi = np.repeat(np.arange(np.size(radius_steps)),
